601c1607a6b922ffd93eade0d1eb0bba1a89767f
IsaacPhiri <isaacphiri315@gmail.com>
2022-07-10 14:50:39 -0700

    Added a function that converts arguments to bases

--- convertNums.c
+++ convertNums.c
@@ -2,24 +2,24 @@
 
 /**
  * convert - converts numbers.
- * @num: unsigned int
- * @base: base to convert to
+ *
  * Return: pointer to buffer.
  */
 
 char *convert(unsigned int num, int base)
 {
-	static const char Representation[] = "0123456789ABCDEF";
-	static char buffer[50];
-	char *ptr;
+        static char Representation[] = "0123456789ABCDEF";
+        static char buffer[50];
+        char *ptr;
 
-	ptr = &buffer[49];
-	*ptr = '\0';
+        ptr = &buffer[49];
+        *ptr = '\0';
 
-	do {
-		*--ptr = Representation[num % base];
-		num /= base;
-	} while (num != 0);
+        do
+        {
+                *--ptr = Representation[num%base];
+                num /= base;
+        }while(num != 0);
 
-	return (ptr);
+        return(ptr);
 }
