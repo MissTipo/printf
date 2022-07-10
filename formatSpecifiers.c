e3da3ab4a26e970e29601eba832f5db782d6dad7
IsaacPhiri <isaacphiri315@gmail.com>
2022-07-10 16:12:20 -0700

    Edited errors in file

--- formatSpecifiers.c
+++ formatSpecifiers.c
@@ -8,7 +8,7 @@
 
 int print_char(va_list list)
 {
-	_write_char(va_arg(list, int));
+	_putchar(va_arg(list, int));
 	return (1);
 }
 
@@ -27,7 +27,7 @@ int print_string(va_list list)
 	if (str == NULL)
 		str = "(null)";
 	for (i = 0; str[i] != '\0'; i++)
-		_write_char(str[i]);
+		_putchar(str[i]);
 	return (i);
 }
 
@@ -39,7 +39,7 @@ int print_string(va_list list)
 
 int print_percent(__attribute__((unused))va_list list)
 {
-	_write_char('%');
+	_putchar('%');
 	return (1);
 }
 
